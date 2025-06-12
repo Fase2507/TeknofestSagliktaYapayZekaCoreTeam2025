2a7bcd0f5c2136dba4a45d5cec9e8247e6e2a3b0
Fase2507 <fatihsever2002@gmail.com>
2025-06-13 00:19:11 +0300

    Temel olarak dicom dosyasi incelendi

--- index_1.py
+++ index_1.py
@@ -1,3 +1,11 @@
+#YORUM VE BILGILENDRME 13.06.2025
+#DICOM dosyalarini micro dicom viewer ile okuyabilirsiniz.
+#Ornek bir dicom dosyasini alip sayisal bir diziye cevirdim sonra normalize ettim
+#Once virgullu sayi formuna sonra da pozitif forma soktum ve 8 bit bir resim olarak kaydettim .
+#Bu forma soktuktan sonra pillow_img.show() ile resim olarak goruntuleyebilirsiniz.
+#AMA burada cok buyuk detay kaybi oluyor bu da istenilen sonuclari vermeyebilir.
+#surec icinde daha cok iyilestirme icin calisacagim.
+
 import numpy as np
 from mpmath.libmp import normalize
 from pydicom import dcmread
@@ -21,11 +29,12 @@ dc_arr=dc.pixel_array
 #NORMALIZE DICOM FILE NUMERICALLY
 float_dcom_arr=dc_arr.astype(float)#Dicom array converted to float
 positive_dcom_arr=np.maximum(float_dcom_arr,0)#now positive numbers
+
 normalized_dcom_arr=positive_dcom_arr/positive_dcom_arr.max()
-normalized_dcom_arr *= 255.0#not 512 anymore
+normalized_dcom_arr *= 255.0
+
 uint8_img=np.uint8(normalized_dcom_arr)#now 8 bit image
 pillow_img=Image.fromarray(uint8_img)
-# foo= np.unique(uint8_img)
+# foo= np.unique(uint8_img) #you can check the numeric values of image and unique values
 pillow_img.show()#there's big loss compared to original image
-print(pillow_img)
 
